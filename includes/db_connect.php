81c304cc1b9b9800dcaf96307d1e9ba533cc06bf
Shaikngit <shaikn@microsoft.com>
2025-01-11 20:48:48 +0530

    Update SSL certificate path in database connection

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -7,7 +7,7 @@
     // $conn = mysqli_connect($servername, $username, $password, $database);
 
     $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($con,NULL,NULL, "/home/site/wwwroot/ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($con, $servername, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
     
     // if (mysqli_connect_errno())
