f08ca5359b9ce4c8b41110d197e29c59e14e73ec
Shaikngit <shaikn@microsoft.com>
2025-01-11 18:30:49 +0530

    Refactor database connection to use environment variables for improved security

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,12 +1,11 @@
 <?php 
-    $servername="localhost";
-    $username="root";
-    $password="";
-    $database="coffeeshop";
+    $servername = getenv('DB_SERVERNAME');
+    $username = getenv('DB_USERNAME');
+    $password = getenv('DB_PASSWORD');
+    $database = getenv('DB_DATABASE');
 
-    $conn = mysqli_connect($servername,$username,$password,$database);
+    $conn = mysqli_connect($servername, $username, $password, $database);
     
-    if(mysqli_connect_errno())
-       die("Connection failed!".mysqli_connect_error());
+    if (mysqli_connect_errno())
+        die("Connection failed: " . mysqli_connect_error());
 ?>
-
