0f237b9b0ff6d94fbcb77639f776811f3c095aeb
Shaikngit <shaikn@microsoft.com>
2025-01-11 20:59:16 +0530

    Fix variable name for database connection in SSL setup

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -6,9 +6,9 @@
 
     // $conn = mysqli_connect($servername, $username, $password, $database);
 
-    $con = mysqli_init();
+    $conn = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "/home/site/wwwroot/ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($con, $servername, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, $servername, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
     
     // if (mysqli_connect_errno())
     //     die("Connection failed: " . mysqli_connect_error());
