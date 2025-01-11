517fd88deaaf9ba10aeb454aaadb778c4e6d3eec
Shaikngit <shaikn@microsoft.com>
2025-01-11 20:39:41 +0530

    Implement SSL connection for database and update connection handling

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -4,8 +4,12 @@
     $password = getenv('DB_PASSWORD');
     $database = getenv('DB_DATABASE');
 
-    $conn = mysqli_connect($servername, $username, $password, $database);
+    // $conn = mysqli_connect($servername, $username, $password, $database);
+
+    $con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($con, $servername, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
     
-    if (mysqli_connect_errno())
-        die("Connection failed: " . mysqli_connect_error());
+    // if (mysqli_connect_errno())
+    //     die("Connection failed: " . mysqli_connect_error());
 ?>
